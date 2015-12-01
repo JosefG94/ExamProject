06a36eaca28ca3601dfea421410afddf50096bf1
Darwiish <inaaboowe@gmail.com>
2015-12-01 11:11:17 +0100

    Take

---
+++ MovieJournalAPI/MovieJournalDAL/Repository/MovieOnListRepository.cs
@@ -0,0 +1,62 @@
+﻿using MovieJournalDAL.Context;
+using MovieJournalDAL.Model;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieJournalDAL.Repository
+{
+    class MovieOnListRepository
+    {
+        public IList<MovieOnList> ReadAll()
+        {
+            using (var ctx = new MovieContext())
+            {
+                return ctx.MoviesOnList.Include(x => x.Profile).ToList();
+            }
+        }
+        public MovieOnList Get(int id)
+        {
+            using (var ctx = new MovieContext())
+            {
+                return ctx.MoviesOnList.Include(x => x.Profile).Where(x => x.Id == id).FirstOrDefault();
+            }
+        }
+        public void Add(MovieOnList movieOnList)
+        {
+            using (var ctx = new MovieContext())
+            {
+                ctx.MoviesOnList.Add(movieOnList);
+                ctx.SaveChanges();
+            }
+        }
+        public void Edit(MovieOnList movieOnList)
+        {
+            using (var ctx = new MovieContext())
+            {
+                MovieOnList m = ctx.MoviesOnList.Where(x => x.Id == movieOnList.Id).First();
+                m.Id = movieOnList.Id;
+                m.Rating = movieOnList.Rating;
+                m.Review = movieOnList.Review;
+                m.Watched = movieOnList.Watched;
+                m.Profile = movieOnList.Profile;
+                ctx.SaveChanges();
+            }
+        }
+        public void Delete(int id)
+        {
+            using (var ctx = new MovieContext())
+            {
+                MovieOnList m = ctx.MoviesOnList.Where(x => x.Id == id).First();
+                if (m != null)
+                    ctx.MoviesOnList.Remove(m);
+                ctx.SaveChanges();
+            }
+        }
+
+    }
+}
+
+
--- MovieJournalAPI/MovieJournalDAL/Repository/ProfileRepository.cs
+++ MovieJournalAPI/MovieJournalDAL/Repository/ProfileRepository.cs
@@ -14,32 +14,33 @@ namespace MovieJournalDAL.Repository
         {
             using (var ctx = new MovieContext())
             {
-                return ctx.Profiles.Include("Title").ToList();
+                return ctx.Profiles.ToList();
             }
         }
         public Profile Get(int id)
         {
             using (var ctx = new MovieContext())
             {
-                return ctx.Profiles.Include("Title").FirstOrDefault();
+                return ctx.Profiles.Where(x => x.Id == id).FirstOrDefault();
             }
         }
-        public void Add(Profile movie)
+        public void Add(Profile profile)
         {
             using (var ctx = new MovieContext())
             {
-                ctx.Profiles.Add(movie);
+                ctx.Profiles.Add(profile);
                 ctx.SaveChanges();
             }
         }
-        public void Edit(Profile movie)
+        public void Edit(Profile profile)
         {
             using (var ctx = new MovieContext())
             {
-                Profile m = ctx.Profiles.Where(x => x.Id == movie.Id).First(); 
-                //insert something here
-               
-              
+                Profile m = ctx.Profiles.Where(x => x.Id == profile.Id).First();
+                m.Id = profile.Id;
+                m.Name = profile.Name;
+                m.MovieOnList = profile.MovieOnList;
+                m.Profiles = profile.Profiles;
                 ctx.SaveChanges();
             }
         }
@@ -55,5 +56,3 @@ namespace MovieJournalDAL.Repository
         }
     }
 }
-
-
