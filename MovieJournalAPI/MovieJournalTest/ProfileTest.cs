a3ebf3b97689611766066fe2d7ce822ffad18422
Darwiish <inaaboowe@gmail.com>
2015-12-09 10:09:31 +0100

    jkhui

---
+++ MovieJournalAPI/MovieJournalTest/ProfileTest.cs
@@ -0,0 +1,29 @@
+﻿using MovieJournalDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MovieJournalTest
+{
+    [TestFixture]
+    public class ProfileTest
+    {
+        Facade facade = new Facade();
+
+
+        [Test]
+        public void Test_Get_Profile()
+        {
+
+            var pro = facade.GetProfileRepository().Get(1);
+
+            Assert.AreEqual(pro.Id, "1");
+            Assert.AreEqual(pro.Name, "Abdirahman");
+            Assert.AreEqual(pro.MovieOnList, "102");
+
+        }
+    }
+}
